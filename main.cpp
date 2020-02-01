ea9bc2193124a85781aef0d348a18d6fc142b4b0
Hue Truong <htylertruong@gmail.com>
2020-02-01 15:00:29 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: huetruong
+ *
+ * Created on February 1, 2020, 2:55 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World";
+    return 0;
+}
+
