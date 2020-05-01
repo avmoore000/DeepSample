877e9d2ef33a4d46003530ad35745ffaa917c1e1
Andrew Moore <av.moore000@gmail.com>
2020-05-01 12:21:16 -0400

    Added script for sample generation

---
+++ sampleRun.sh
@@ -0,0 +1,3 @@
+./a.out /Volumes/FetaMozzExt/FreeMusic2010Results /Volumes/FetaMozzExt/FreeMusic2010 FreeMusic2010Log 2 1 1 1 1
+
+./a.out	/Volumes/FetaMozzExt/FreeMusic2011Results /Volumes/FetaMozzExt/FreeMusic2011 FreeMusic2011Log 2 1 1 1 1 
