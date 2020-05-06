c00a2fd23575534d12c2288e56d29af3b4930c1b
Andrew Moore <av.moore000@gmail.com>
2020-05-06 18:54:42 -0400

    Delete sampleRun.sh

--- sampleRun.sh
+++
@@ -1,3 +0,0 @@
-./a.out /Volumes/FetaMozzExt/FreeMusic2010Results /Volumes/FetaMozzExt/FreeMusic2010 FreeMusic2010Log 2 1 1 1 1
-
-./a.out	/Volumes/FetaMozzExt/FreeMusic2011Results /Volumes/FetaMozzExt/FreeMusic2011 FreeMusic2011Log 2 1 1 1 1 
